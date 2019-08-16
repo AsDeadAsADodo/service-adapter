8e2d26284529939ce198b020ac876e4ce31d8b0c
kongxiangji <kongxiangji@yuerushui.com>
2019-08-16 16:14:10 +0800

    add TopService

---
+++ src/main/java/core/TopService.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+  * @Description:   abstract message set method
+  * @Author: EM
+  * @Date: 2019/8/12-17:48
+  */
+public interface TopService {
+
+    String getSignature();
+}
